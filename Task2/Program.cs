1f3f7027b1c2500a0c7f90d8f80afe390bc9d2ee
AysunGadimli <aysungg@code.edu.az>
2024-08-21 02:34:38 +0400

    update code

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,44 +1,48 @@
-﻿using System.Security.Cryptography;
-
-namespace Task2;
+﻿namespace Task2;
 
 class Program
 {
     static void Main(string[] args)
     {
-       // a.Verilmish ededin menfi ve ya musbet oldugunu tapan algorithm
+        // a.Verilmish ededin menfi ve ya musbet oldugunu tapan algorithm
+
+
+        int num = 7;
+        if (num > 0)
+        {
+            Console.WriteLine("müsbet");
+        }
+        else
+        {
+            Console.WriteLine("menfi");
+        }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        int num2 = -7;
+        if (num < 0)
+        {
+            Console.WriteLine("menfi");
+        }
+        else
+        {
+            Console.WriteLine("musbet");
+        }
 
 
 
         //b.Verilmish ededin hem 3 - e hem de 7 - e tam bolunub bolunmediyini tapan algorithm
 
 
-
-
-
-
-
-
-
-
-
-
-
+        int num3 = 21;
+        if (num3 % 3==0 && num3 % 7==0)   
+        {
+            Console.WriteLine("tam bolunur");
+        }
+        else
+        {
+            Console.WriteLine("tam bolunmur");
+        }
 
     }
 }
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a9dcd23230f658f1fa3f37dc47862ddc3add0c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa9fc963f0821f9e6fa71a99e91bf2b7f960e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
